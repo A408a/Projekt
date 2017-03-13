985e841f9f8061f136abd563eded57922cb5d2f0
Landi29 <amoha16@student.aau.dk>
2017-03-13 19:17:52 +0100

    Tilføjet en liste til stopwords og shingles, og begyndt på en metode

---
+++ Jaccardalgoritme/Jaccardalgoritme/JaccardSimilarity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaccardalgoritme
+{
+    class JaccardSimilarity
+    {
+        /*Define method for shingling.
+         * The method must find shingles from a document.
+         * A shingle is set to a sentece, which consist of a stopword and the two words that follows the word. 
+         * */
+        // Making a List of strings which consists of stopwords. 
+        public List<string> stopWords = System.IO.File.ReadAllLines(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords2.txt").ToList();
+
+        // Making a List of strings which will be used to store shinglevalues from a document. 
+        public List<string> shingles = new List<string>();
+
+        /*Define method for shingling.
+        * The method must find shingles from a document.
+        * A shingle is set to a sentece, which consist of a stopword and the two words that follows the word. 
+        * */
+        public void GetShingles(string tekst)
+        {
+           
+
+        }
+          
+    }
+}
---
+++ Jaccardalgoritme/Jaccardalgoritme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoadTextLibrary;
+
+namespace Jaccardalgoritme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string text = System.IO.File.ReadAllText(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords.txt");
+            string nytekst = text.Replace(", ", Environment.NewLine);
+            Console.ReadLine();
+            System.IO.File.WriteAllText(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords2.txt", nytekst);
+            */
+
+           
+
+            /*LoadEachWordToList tekstA = new LoadEachWordToList(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords.txt");
+            tekstA.LoadText();
+            tekstA.GetEachWordInText();*/
+            Console.ReadLine();
+
+        }
+    }
+}
