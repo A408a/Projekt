fbd67f48ec227e3532265d06eccbefd46bdf7c2e
Landi29 <amoha16@student.aau.dk>
2017-03-14 14:54:25 +0100

    The GetShingles method has been defined

--- Jaccardalgoritme/Jaccardalgoritme/JaccardSimilarity.cs
+++ Jaccardalgoritme/Jaccardalgoritme/JaccardSimilarity.cs
@@ -8,10 +8,7 @@ namespace Jaccardalgoritme
 {
     class JaccardSimilarity
     {
-        /*Define method for shingling.
-         * The method must find shingles from a document.
-         * A shingle is set to a sentece, which consist of a stopword and the two words that follows the word. 
-         * */
+      
         // Making a List of strings which consists of stopwords. 
         public List<string> stopWords = System.IO.File.ReadAllLines(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords2.txt").ToList();
 
@@ -22,9 +19,26 @@ namespace Jaccardalgoritme
         * The method must find shingles from a document.
         * A shingle is set to a sentece, which consist of a stopword and the two words that follows the word. 
         * */
-        public void GetShingles(string tekst)
+
+        public void GetShingles(List<string> tekst)
         {
-           
+            // count variable. 
+            int i = 0;
+            // foreach loop that iterates over the text. 
+            foreach (string item in tekst)
+            {
+                
+                if (stopWords.Contains(item.ToLower()))
+                {
+                    shingles.Add(item + " " + tekst[i+1] + " " + tekst[i+2]);
+                }
+
+                ++i;
+                // if the count variable is 2 less than the number of elements in the list, then the loop is finished. 
+                // The reason for this is that the last index number in the list is the number of elements -1. 
+                if (i == (tekst.Count - 2))
+                    break;
+            }
 
         }
           
--- Jaccardalgoritme/Jaccardalgoritme/Program.cs
+++ Jaccardalgoritme/Jaccardalgoritme/Program.cs
@@ -17,8 +17,30 @@ namespace Jaccardalgoritme
             Console.ReadLine();
             System.IO.File.WriteAllText(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords2.txt", nytekst);
             */
+            
+            // Creating an instance of the LoadEachWordToList class. 
+            LoadEachWordToList tekstB = new LoadEachWordToList(@"C:\Users\Aryan\Dropbox\P1 Projekt\P2\Program\Nyheder_Database\Pizzagate1.txt");
 
-           
+            //tekstB.LoadText();
+
+            //foreach (string i in tekstB.Words)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // Creating an instance of the JaccardSimilarity class called nytekst.
+            JaccardSimilarity nytekst = new JaccardSimilarity();
+
+            /* Calling the GetShingles method from the JaccardSimilarity class on the object.
+             *  The parameter is a text, where each word is an element in a List. 
+             *  */
+            nytekst.GetShingles(tekstB.Words);
+
+            foreach (string item in nytekst.shingles)
+            {
+                Console.WriteLine(item);
+            }
+
 
             /*LoadEachWordToList tekstA = new LoadEachWordToList(@"C:\Users\Aryan\Dropbox\P1 projekt\P2\Program\Jaccard\stopwords.txt");
             tekstA.LoadText();
