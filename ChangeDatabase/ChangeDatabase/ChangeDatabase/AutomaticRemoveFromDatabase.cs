68e083e09bca345001bc632adc9f2c5f2002cd0a
Jesper Skovby <Jesper Skovby>
2017-04-19 11:10:36 +0200

    sjove ting

--- ChangeDatabase/ChangeDatabase/ChangeDatabase/AutomaticRemoveFromDatabase.cs
+++ ChangeDatabase/ChangeDatabase/ChangeDatabase/AutomaticRemoveFromDatabase.cs
@@ -21,20 +21,54 @@ namespace ChangeDatabase
 {
     class AutomaticRemoveFromDatabase
     {
-
-        //        public AutomaticRemoveFromDatabase(){        }
-        public void FindOutdatedFile()
+        
+        public void FindOutdateFolder()
         {
             string DirectoryName = "Testfolder";
             string Path = Directory.GetCurrentDirectory();
+            
             // removes \ChangeDatabase\bin\Debug from directory.
-            Path = Path.Remove(Path.Length - 25);
+            Path = Path.Remove(Path.Length - 25); //DETTE SKAL OPTIMERES OG IKKE EKSPLICIT. ****
             Path = System.IO.Path.Combine(Path, DirectoryName);
-            Console.WriteLine(Directory.GetFiles(Path));
-            Console.WriteLine(Path);
-            Console.ReadLine();
+            DirectoryInfo Dir = new DirectoryInfo(Path);
 
+            foreach (FileInfo file in Dir.GetFiles())
+                FindOutdatedFile(file.ToString(), file);
         }
 
+        private void FindOutdatedFile(string File, FileInfo FileInformation)
+        {
+            try
+            {
+                int month = Int32.Parse(File.Substring(3, 2));
+                int year = Int32.Parse(File.Substring(6, 4));
+                // If current year is equal to article year
+                if (year == DateTime.Now.Year)
+                {
+                    // If article is less than 3 months old
+                    if (month > DateTime.Now.Month - 3)
+                        Console.WriteLine(File);
+                    else
+                        RemoveFile(FileInformation);
+                }
+                else
+                {
+                    RemoveFile(FileInformation);
+                }
+
+                Console.ReadLine();
+            }
+
+            catch (FormatException)
+            {
+                // month or year was not converted to int. It will simply be ignored.
+            }
+        }
+
+        private void RemoveFile(FileInfo FileInformation)
+        {
+            // SKAL VI HAVE EN "FOUND X OUTDATED ARTICLE. SHOULD THEY BE REMOVED? "YES" "NO?"
+            FileInformation.Delete();
+        }
     }
 }
