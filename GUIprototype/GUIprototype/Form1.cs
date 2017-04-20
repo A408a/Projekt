6110c483531f9e2cf094d1a0e5530808e48bd127
Landi29 <amoha16@student.aau.dk>
2017-04-20 19:53:21 +0200

    Nu kan der laves nye forms

--- GUIprototype/GUIprototype/Form1.cs
+++ GUIprototype/GUIprototype/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JaccardSimilarityLibrary;
 
 namespace GUIprototype
 {
@@ -20,6 +21,14 @@ namespace GUIprototype
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void checkText_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CheckText CheckText = new CheckText();
+            CheckText.Show();
+            
         }
     }
 }
