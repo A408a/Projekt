45916cdc74a249a2aef1f2e70a07f83858381f2b
Landi29 <amoha16@student.aau.dk>
2017-04-21 14:40:12 +0200

    Added resource file again

---
+++ GUIprototype/GUIprototype/CheckLinkResult.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace GUIprototype
+{
+    partial class CheckLinkResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Messagelabel = new System.Windows.Forms.Label();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Messagelabel
+            // 
+            this.Messagelabel.AutoSize = true;
+            this.Messagelabel.Location = new System.Drawing.Point(21, 41);
+            this.Messagelabel.Name = "Messagelabel";
+            this.Messagelabel.Size = new System.Drawing.Size(226, 26);
+            this.Messagelabel.TabIndex = 0;
+            this.Messagelabel.Text = "The link does not lead to a trustworthy site.\r\nDo you want to continue by extract" +
+    "ing the file?";
+            this.Messagelabel.Click += new System.EventHandler(this.Messagelabel_Click);
+            // 
+            // continueButton
+            // 
+            this.continueButton.Location = new System.Drawing.Point(103, 138);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(75, 23);
+            this.continueButton.TabIndex = 1;
+            this.continueButton.Text = "Continue";
+            this.continueButton.UseVisualStyleBackColor = true;
+            // 
+            // CheckLinkResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.Messagelabel);
+            this.Name = "CheckLinkResult";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckLinkResult";
+            this.Load += new System.EventHandler(this.CheckLinkResult_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Messagelabel;
+        private System.Windows.Forms.Button continueButton;
+    }
+}
