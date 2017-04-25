ffa849e5f757b48acd25bed664f69a283dc9822a
Jesper Skovby <Jesper Skovby>
2017-04-25 12:48:48 +0200

    ChangeDatabase uden tests

--- ChangeDatabase/ChangeDatabase/ChangeDatabase/Program.cs
+++ ChangeDatabase/ChangeDatabase/ChangeDatabase/Program.cs
@@ -11,7 +11,7 @@ namespace ChangeDatabase
     {
         static void Main(string[] args)
         {
-            int start = 1;
+            int start = 0;
             if (start == 1)
             {
                 AutomaticRemoveFromDatabase Run = new AutomaticRemoveFromDatabase();
@@ -19,11 +19,12 @@ namespace ChangeDatabase
             }
             else
             {
-                ChangeDatabase Run1 = new ChangeDatabase("test.txt");
-                Run1.RemoveArticle();
                 LoadEachWordToList TextA = new LoadEachWordToList(@"C:\Users\Jesper\Dropbox\AAU\P2\Program\Nyheder_Database\Pizzagate2.txt");
                 ChangeDatabase Run = new ChangeDatabase(TextA.Words);
                 Run.AddArticle();
+                Console.ReadLine();
+                ChangeDatabase Run1 = new ChangeDatabase("test.txt");
+                Run1.RemoveArticle();
             }
         }
     }
