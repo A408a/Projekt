2118ef092dd94a9ea0b7b3c4e6f9616e77e9aac5
Dalgas104 <simon-dalgas@fiberpost.dk>
2017-05-10 12:04:41 +0200

    Kommentare i WebTotext

--- WebArticleURLToText/WebArticleURLToText/NewsPage.cs
+++ WebArticleURLToText/WebArticleURLToText/NewsPage.cs
@@ -8,23 +8,24 @@ namespace WebArticleURLToText
 {
     public class NewsPage
     {
-        private static Dictionary<string, Tuple<string, string>> websites = new Dictionary<string, Tuple<string, string>>();  // Instance
-
-        private HtmlDocument doc;         // Initialization
-        private string domain;              // Initialization
+        //Dictionary comtainning information partating to the way we read a HTML site/article for a specific newssite 
+        private static Dictionary<string, Tuple<string, string>> websites = new Dictionary<string, Tuple<string, string>>();
+        //HTMLDocument: Provides top-level programmatic access to a HTML document hosted by the WebBrowser control.
+        private HtmlDocument doc;
+        private string domain;           
         public string Headline { get; private set; }
         public List<string> BodyText = new List<string>();
 
         public NewsPage(string webaddress)   
         {
             InitializeWebsite();
-            doc = GetWebsite(webaddress);      // Initialization
+            doc = GetWebsite(webaddress);    
             GetArticleHead();
             GetArticleBody();
         }
 
-
-        private HtmlDocument GetWebsite(string webaddress)  // Method that downloads the string from website.
+        // Method that downloads the HTMLcode from website.
+        private HtmlDocument GetWebsite(string webaddress)  
         {
             if (webaddress == null)
             {
@@ -33,50 +34,69 @@ namespace WebArticleURLToText
             else
             {
                 HtmlDocument doc1 = new HtmlDocument();
-                string websiteString = new WebClient().DownloadString(webaddress); //WebException
+                //Intanciates a WebClient and specifies the encoding type to UTF8 to prevent weird characters
+                WebClient client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
+                //Fetches Html document of the webite
+                string websiteString = client.DownloadString(webaddress);          
+                //Loads the websiteString to a HtmlDocument type              
                 doc1.LoadHtml(websiteString);
+                //Gets the host name of a website eks. "http:/www.123.com/1341341 --> www.123.com
                 domain = new Uri(webaddress).Host;
 
                 return doc1;
             }
         }
 
-        private void GetArticleHead()   // Methode: Gets the article title.
-        {
-            foreach (HtmlNode node in GetArticleNode().SelectNodes("//h1"))
-            {
-                Headline = node.InnerText + "\n";
-            }
-        }
-
-        private void GetArticleBody()   // Methode: Gets the article content.
-        {
-            foreach (HtmlNode node in GetArticleNode().SelectNodes("//p"))
-            {
-                if (node.ChildAttributes("class").Count() != 0)
-                {
-                    continue;
-                }
-                BodyText.Add(node.InnerText);
-            }
-        }
-
-        private HtmlNode GetArticleNode()    // Methode that gets article nods - (A node is any piece of individual content)
+        // Methode that gets article nods - (A node is any piece of individual content)
+        private HtmlNode GetArticleNode()    
         {
             if (domain == null)
                 throw new ArgumentNullException("Input is null");
             else
             {
-                Tuple<string, string> vals = websites[domain]; //KeyNotFoundException
-
-                return doc.GetElementbyId(vals.Item1).SelectNodes(vals.Item2).Last();    // Gets ID and div.
+                //Gets the Xpath from the dictionary
+                //A turple contains a set of values in this case 2
+                Tuple<string, string> vals = websites[domain];
+                //GetElementById: Retrieves a single HtmlElement using the element's ID attribute as a search key.
+                //SelectNodes: Return a list of nodes matching the Xpath expression, but we only want the nodes at the end of the path -> Last();
+                return doc.GetElementbyId(vals.Item1).SelectNodes(vals.Item2).Last(); 
             }
-
         }
 
+        // Methode: Gets the article title.
+        private void GetArticleHead()   
+        {
+            //We have one node which is converted to a list of childnodes wherein
+            //we search nodes classified with h1. h1: Headline 1, the biggest headline in Htmlcode  
+            foreach (HtmlNode node in GetArticleNode().SelectNodes("//h1"))
+            {
+                //Gets the InnerText inside Html-tags <h1>Innertext</h1>
+                Headline = node.InnerText + "\n";
+            }
+        }
+
+        // Methode: Gets the article content.
+        private void GetArticleBody()   
+        {
+            //We have one node which is converted to a list of childnodes wherein
+            //we search nodes classified with p. p: paragraph, for regular text in Htmlcode  
+            foreach (HtmlNode node in GetArticleNode().SelectNodes("//p"))
+            {
+                //If the node is a p class node it ignored
+                if (node.ChildAttributes("class").Count() != 0)
+                {
+                    continue;
+                }
+                //Gets the InnerText inside Html-tags <p>Innertext</p> and adds it to a list
+                BodyText.Add(node.InnerText);
+            }
+        }
+
+        //Initiaizes preinserted newssites 
         private void InitializeWebsite()
         {
-            // Webpage name, ID(Potential) and div[]
+            //Dictionary(Key: Webpage hostname, Value: Turple(Xpath))
+            //Xpath: Contains the id/pointer for a newsspecific overall <div class> and a path to where the body text is located
             websites.Add("www.bbc.com", Tuple.Create("page", "//div[2]/div[2]/div/div[1]/div[1]"));
             websites.Add("www.theguardian.com", Tuple.Create("article", "//div[4]/div/div[1]/div[5]"));
             websites.Add("news.sky.com", Tuple.Create("", "//html/body/div[1]/div[2]"));
