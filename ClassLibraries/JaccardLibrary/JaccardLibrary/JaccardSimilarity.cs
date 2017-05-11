c14abc373f766e7303f8f0a68773d9dde4ef61a4
Patrick Tietze <patrick.tietze@outlook.dk>
2017-05-11 09:36:46 +0200

    Exceptions in Cosine and Jaccard library updated

--- ClassLibraries/JaccardLibrary/JaccardLibrary/JaccardSimilarity.cs
+++ ClassLibraries/JaccardLibrary/JaccardLibrary/JaccardSimilarity.cs
@@ -7,29 +7,38 @@ using JaccardLibrary.Properties;
 
 namespace JaccardSimilarityLibrary
 {
-    public class JaccardSimilarity
+    public class JaccardSimilarity 
     {
         #region Members
-        public string[] tempStopWords; 
-        public HashSet<string> stopWords = new HashSet<string>(); // Contains the stopwords from tempStopWords
-        public HashSet<string> shinglesA = new HashSet<string>(); // Contains shingles from TextA 
-        public HashSet<string> shinglesB = new HashSet<string>(); // Contains shingles from TextB
-        public List<string> TextA = new List<string>(); // Contains the first text 
-        public List<string> TextB = new List<string>(); // Contains the second text 
-        public double Similarity; // Contains the JaccardSimilarity between the two texts  
+        public string[] tempStopWords { get; private set; }
+        public HashSet<string> stopWords { get; private set; } = new HashSet<string>(); // Contains the stopwords from tempStopWords
+        public HashSet<string> shinglesA { get; private set; } = new HashSet<string>(); // Contains shingles from TextA 
+        public HashSet<string> shinglesB { get; private set; } = new HashSet<string>(); // Contains shingles from TextB
+        public List<string> TextA { get; private set; } = new List<string>(); // Contains the first text 
+        public List<string> TextB { get; private set; } = new List<string>(); // Contains the second text 
+        public IEnumerable<string> SimilarElements { get; private set; }
+        public IEnumerable<string> AllElements { get; private set; }
+        public decimal Similarity { get; private set; } // Contains the JaccardSimilarity between the two texts  
         #endregion  
 
         public JaccardSimilarity(List<string> TextA, List<string> TextB)
         {
             tempStopWords = Resources.stopwords2.Split(new string[] { Environment.NewLine }, StringSplitOptions.None); // Splits stopwords into array 
             CreateHashSetOfStopWords(); // Creates a HashSet of stopwords 
+
             this.TextA = TextA;
             this.TextB = TextB;
+
+            if (!TextA.Any())
+                throw new EmptyTextException("The first text is empty");
+
+            if (!TextB.Any())
+                throw new EmptyTextException("The second text is empty");
 
             GetShingles(TextA, shinglesA); // Get shingles from TextA
             GetShingles(TextB, shinglesB); // Get shingles from TextB
 
-            Similarity = CalculateSimilarity(shinglesA, shinglesB); // Calculates similarity between the two texts 
+            Similarity = CalculateSimilarity(); // Calculates similarity between the two texts 
         }
 
         private void CreateHashSetOfStopWords() 
@@ -42,35 +51,34 @@ namespace JaccardSimilarityLibrary
 
         private void GetShingles(List<string> tekst, HashSet<string> shingles) 
         {
-               
             for(int i = 0; i < tekst.Count; ++i) // Iterates through the text 
             {
                 if (stopWords.Contains(tekst[i].ToLower())) // Happens if the current word is a stopword
 
-                    shingles.Add(tekst[i] + " " + tekst[i + 1] + " " + tekst[i + 2]); // Add a shingle (stopword + the following two words) 
+                    shingles.Add($"{tekst[i]} {tekst[i + 1]} {tekst[i + 2]}".ToLower()); // Add a shingle (stopword + the following two words) 
 
                 if (i == (tekst.Count - 3)) // Happens if there's not enough words left to make a shingle (3 words are needed to make a shingle)
                     break;
             }
         }
 
-        private double CalculateSimilarity(HashSet<string> shingleSetOne, HashSet<string> shingleSetTwo)
+        private decimal CalculateSimilarity()
         {
-            IEnumerable<string> similarElements = shingleSetOne.Intersect(shingleSetTwo); // Gets the Intersections of the two HashSets of shingles  
+            if (!shinglesA.Any() || !shinglesB.Any()) 
+                return 0; 
 
-            IEnumerable<string> allElements = shinglesA.Union(shinglesB); // Gets the Union of the two HashSets of shingles  
+            SimilarElements = shinglesA.Intersect(shinglesB); // Gets the Intersections of the two HashSets of shingles  
+
+            AllElements = shinglesA.Union(shinglesB); // Gets the Union of the two HashSets of shingles  
 
             // Number og elements in the Intersection devided by number of elements in the Union
-            double jaccardValue = (double)similarElements.Count() / (double)allElements.Count(); 
-
-            return jaccardValue;
+            return (decimal)SimilarElements.Count() / (decimal)AllElements.Count(); 
         }
 
         public void Print()
         {
             Console.WriteLine($"Jaccard similarity: {Similarity}");
         }
-           
     }  
 }
 
